2be7f6a15446b75bef4b71523548f657de150f81
lpiem <lpiem@iutabgdin202-02.univ-lyon1.fr>
2019-11-12 16:33:46 +0100

    v1.4

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,11 @@
 $app->get('/users/edit/{id}', 'App\Controller\UsersController::editAction')->bind('users.edit');
 $app->get('/users/new', 'App\Controller\UsersController::newAction')->bind('users.new');
 $app->post('/users/delete/{id}', 'App\Controller\UsersController::deleteAction')->bind('users.delete');
-$app->post('/users/save', 'App\Controller\UsersController::saveAction')->bind('users.save');*/
+$app->post('/users/save', 'App\Controller\UsersController::saveAction')->bind('users.save');
+
+$app->get('/pokemon/list', 'App\Controller\PokemonsController::listAction')->bind('pokemon.list');
+
+$app->get('/article/list', 'App\Controller\ArticleController::listAction')->bind('article.list');*/
 
 $app->get('/users/list', 'App\Users\Controller\UsersController::listAction')->bind('users.list');
 $app->get('/users/edit/{id}', 'App\Users\Controller\UsersController::editAction')->bind('users.edit');
@@ -12,6 +16,16 @@ $app->get('/users/new', 'App\Users\Controller\UsersController::newAction')->bind
 $app->post('/users/delete/{id}', 'App\Users\Controller\UsersController::deleteAction')->bind('users.delete');
 $app->post('/users/save', 'App\Users\Controller\UsersController::saveAction')->bind('users.save');
 
-$app->get('/pokemon/list', 'App\Controller\PokemonsController::listAction')->bind('pokemon.list');
+$app->get('/pokemons/list', 'App\Pokemons\Controller\UsersController::listAction')->bind('pokemons.list');
+$app->get('/pokemons/edit/{id}', 'App\Pokemons\Controller\UsersController::editAction')->bind('pokemons.edit');
+$app->get('/pokemons/new', 'App\Pokemons\Controller\UsersController::newAction')->bind('pokemons.new');
+$app->post('/pokemons/delete/{id}', 'App\Pokemons\Controller\UsersController::deleteAction')->bind('pokemons.delete');
+$app->post('/pokemons/save', 'App\Pokemons\Controller\UsersController::saveAction')->bind('pokemons.save');
 
-$app->get('/article/list', 'App\Controller\ArticleController::listAction')->bind('article.list');
+$app->get('/devices/list', 'App\Devices\Controller\UsersController::listAction')->bind('devices.list');
+$app->get('/devices/edit/{id}', 'App\Devices\Controller\UsersController::editAction')->bind('devices.edit');
+$app->get('/devices/new', 'App\Devices\Controller\UsersController::newAction')->bind('devices.new');
+$app->post('/devices/delete/{id}', 'App\Devices\Controller\UsersController::deleteAction')->bind('devices.delete');
+$app->post('/devices/save', 'App\Devices\Controller\UsersController::saveAction')->bind('devices.save');
+
+
