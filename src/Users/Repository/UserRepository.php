2fdb425a9c1af16b0118fd9b0fc0056bdd162db0
lpiem <lpiem@iutabgdin202-02.univ-lyon1.fr>
2020-02-11 13:18:06 +0100

    add response getListPokemonAction

--- src/Users/Repository/UserRepository.php
+++ src/Users/Repository/UserRepository.php
@@ -38,7 +38,7 @@ class UserRepository
        $usersData = $statement->fetchAll();
        foreach ($usersData as $userData) {
            //$userEntityList[$userData['id']] = new User($userData['id'], $userData['login'], $userData['mail'], $userData['password']);
-           $userEntityList[1] = new User("1", "2", "3", "4");
+           $userEntityList[$userData['id']] = new User("1", "2", "3", "4");
        }
        return $userEntityList;
    }
