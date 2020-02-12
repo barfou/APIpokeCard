23eaf012925bd4ee2b7c4de83f657796dbea31ad
lpiem <lpiem@iutabgdin202-02.univ-lyon1.fr>
2020-02-12 12:49:54 +0100

    add Insert & Delete User action

--- src/routes.php
+++ src/routes.php
@@ -13,14 +13,14 @@ $app->get('/article/list', 'App\Controller\ArticleController::listAction')->bind
 
 //User
 $app->get('/users', 'App\Users\Controller\UsersController::getListUserAction')->bind('users.list');
-$app->get('/users/detail', 'App\Users\Controller\UsersController::getUserAction')->bind('users.get');
+$app->get('/users/detail', 'App\Users\Controller\UsersController::getUserAction')->bind('users.entity');
 $app->post('/users/delete', 'App\Users\Controller\UsersController::deleteAction')->bind('users.delete');
-$app->post('/users/save', 'App\Users\Controller\UsersController::saveAction')->bind('users.save');
+$app->post('/users/insert', 'App\Users\Controller\UsersController::insertAction')->bind('users.insert');
 
-$app->get('/users/edit/{id}', 'App\Users\Controller\UsersController::editAction')->bind('users.edit');
+/*$app->get('/users/edit/{id}', 'App\Users\Controller\UsersController::editAction')->bind('users.edit');
 $app->get('/users/new', 'App\Users\Controller\UsersController::newAction')->bind('users.new');
 $app->post('/users/delete/{id}', 'App\Users\Controller\UsersController::deleteAction')->bind('users.delete');
-$app->post('/users/save', 'App\Users\Controller\UsersController::saveAction')->bind('users.save');
+$app->post('/users/save', 'App\Users\Controller\UsersController::saveAction')->bind('users.save');*/
 
 //Pokemon
 $app->get('/pokemons', 'App\Pokemons\Controller\PokemonsController::getListPokemonAction')->bind('pokemons.list');
@@ -28,9 +28,9 @@ $app->get('/pokemons', 'App\Pokemons\Controller\PokemonsController::getListPokem
 $app->get('/pokemons/insertImg', 'App\Pokemons\Controller\PokemonsController::insertImgAction')->bind('pokemons.insrtImg');
 $app->get('/pokemons/detail', 'App\Pokemons\Controller\PokemonsController::getPokemonAction')->bind('pokemons.entity');
 
-$app->get('/pokemons/edit/{id}', 'App\Pokemons\Controller\PokemonsController::editAction')->bind('pokemons.edit');
+/*$app->get('/pokemons/edit/{id}', 'App\Pokemons\Controller\PokemonsController::editAction')->bind('pokemons.edit');
 $app->get('/pokemons/new', 'App\Pokemons\Controller\PokemonsController::newAction')->bind('pokemons.new');
 $app->post('/pokemons/delete/{id}', 'App\Pokemons\Controller\PokemonsController::deleteAction')->bind('pokemons.delete');
-$app->post('/pokemons/save', 'App\Pokemons\Controller\PokemonsController::saveAction')->bind('pokemons.save');
+$app->post('/pokemons/save', 'App\Pokemons\Controller\PokemonsController::saveAction')->bind('pokemons.save');*/
 
 
