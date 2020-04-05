ef87b46f2201cce810e7bd1f7a66dd6fc32dc4ee
ClementB01 <bar.clement01@gmail.com>
2020-04-05 18:15:58 +0200

    Try with

--- src/Users/Controller/UsersController.php
+++ src/Users/Controller/UsersController.php
@@ -68,29 +68,24 @@ class UsersController
     public function insertUserAction(Request $request, Application $app)
     {
         //Create Response object
-        //$response = new Response();
-        //$response->headers->set('Content-Type', 'application/json');
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
 
-        //$parameters = $request;
-        //var_dump($request);
-        var_dump($_POST["login"]);
-        //var_dump($parameters->parameters);
-
-        //$parametersInsert = [
-        //    "login" => $parameters['login'],
-        //    "mail" => $parameters['mail'],
-        //    "password" => $parameters['password']
-        //];
-        //$bool = $app['repository.user']->insert($parametersInsert);
-        //if($bool == true){
-        //    $response->setContent(json_encode("Request executed"));
-        //    $response->setStatusCode(Response::HTTP_OK);
-        //}
-        //else{
-        //    $response->setContent(json_encode("Request not executed"));
-        //    $response->setStatusCode(Response::HTTP_NOT_FOUND);
-        //}
-        //return $response;
+        $parametersInsert = [
+            "login" => $_POST['login'],
+            "mail" => $_POST['mail'],
+            "password" => $_POST['password']
+        ];
+        $bool = $app['repository.user']->insert($parametersInsert);
+        if($bool == true){
+            $response->setContent(json_encode("Request executed"));
+            $response->setStatusCode(Response::HTTP_OK);
+        }
+        else{
+            $response->setContent(json_encode("Request not executed"));
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+        }
+        return $response;
         return "";
     }
 
