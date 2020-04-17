762e85edfc2e1e43e3adabaa29a801d177ac5240
ClementB01 <bar.clement01@gmail.com>
2020-04-17 16:15:52 +0200

    Fix deleteUserAcction

--- src/Pokemons/Repository/PokemonRepository.php
+++ src/Pokemons/Repository/PokemonRepository.php
@@ -89,7 +89,7 @@ class PokemonRepository
         $statement = $queryBuilder->execute();
 
         $countOwnedPokemonsData = $statement->fetchAll();
-        return $countOwnedPokemonsData;
+        return $countOwnedPokemonsData[0];
     }
 
     public function insertOwnedPokemon($parameters)
