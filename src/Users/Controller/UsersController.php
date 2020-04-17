f83d0d623f8c6cfc2a85eeb8d541991cf49eadb2
ClementB01 <bar.clement01@gmail.com>
2020-04-17 16:18:05 +0200

    Fix deleteUserAcction

--- src/Users/Controller/UsersController.php
+++ src/Users/Controller/UsersController.php
@@ -149,8 +149,6 @@ class UsersController
 
         $count = $app['repository.pokemon']->countOwnedPokemon($parameters['id']);
 
-        var_dump($count);
-
         if($count > 0){
             $response->setContent(json_encode("Impossible to delete this user because he is linked to pokemons"));
             $response->setStatusCode(Response::HTTP_FORBIDDEN);
